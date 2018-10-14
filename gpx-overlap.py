d9c74212729ee02064634c52ec505b94f929a903
WanderingStar <github@account.loath.org>
2018-10-14 14:18:49 -0500

    organize by local day mostly working

---
+++ gpx-overlap.py
@@ -0,0 +1,128 @@
+import gpxpy
+import gpxpy.gpx
+from intervaltree import IntervalTree
+import datetime
+import argparse
+import re
+from collections import Counter
+
+pattern_labels = [
+    ("Active Log", "Zumo"),
+    ("Aneel Nazareth", "inReach")
+]
+
+def matching_labels(string):
+    for pattern, label in pattern_labels:
+        if re.search(pattern, string):
+            yield label
+
+def merge_track_segments_within_interval(segments, start, end):
+    print(f"merging {len(segments)} segments\n in {start}, {end} ({(end - start).total_seconds()}s)")
+    for s in segments:
+        tbs = s.get_time_bounds().start_time
+        tbe = s.get_time_bounds().end_time
+        sec = (tbe - tbs).total_seconds()
+        print(f"    {tbs}, {tbe} ({sec}s): {s.get_points_no()}")
+    seg_points = []
+    seg_extensions = []
+    for s in segments:
+        seg_points += s.points
+        seg_extensions += s.extensions
+    in_interval = [p for p in seg_points if p.time >= start and p.time <= end]
+    print(f"    total points {len(seg_points)} -> {len(in_interval)}")
+    merged = gpxpy.gpx.GPXTrackSegment(points=sorted(in_interval, key=lambda p: p.time))
+    merged.extensions = list(set(seg_extensions))
+    print(f"    extensions: {merged.extensions}")
+    return merged
+
+def gpx_merge_intersect(input_gpx):
+    """
+    Given an GPX file containing multiple overlapping tracks, merge the parts
+    that overlap and create separate tracks for the parts that only partially
+    overlap.
+    """
+    intervals = IntervalTree()
+
+    all_points = set()
+
+    for track in input_gpx.tracks:
+        for segment in track.segments:
+            bounds = segment.get_time_bounds()
+            start = bounds.start_time.timestamp()
+            end = bounds.end_time.timestamp()
+            if start == end:
+                end = end + 1
+            print(f"Found track {track.name}")
+            segment.extensions.append(f"OriginalName:{track.name}")
+            intervals[start:end] = [segment]
+            all_points.update(set([p.time for p in segment.points]))
+
+    n_original = len(intervals)
+    intervals.split_overlaps()
+    for i in intervals:
+        print(i)
+    n_split = len(intervals)
+    intervals.merge_equals(data_reducer=lambda a, b: a + b)
+    n_merged = len(intervals)
+    print(f"Split {n_original} intervals into {n_split} merged into {n_merged}")
+
+    merged_points = set()
+
+    output_gpx = input_gpx.clone()
+    output_gpx.tracks = []
+
+    for i in sorted(intervals):
+        print(i)
+        start = datetime.datetime.fromtimestamp(i.begin)
+        end = datetime.datetime.fromtimestamp(i.end)
+        segments = i.data
+        merged = merge_track_segments_within_interval(segments, start, end)
+        merged_points.update(set([p.time for p in merged.points]))
+
+        original_names = []
+        labels = Counter()
+        custom_names = []  # names that didn't match the label patterns
+        for ext in merged.extensions:
+            match = re.match("OriginalName:(.*)", ext)
+            if match:
+                original_name = match.group(1)
+                original_names.append(original_name)
+                matching = list(matching_labels(original_name))
+                if not matching:
+                    print(f"custom name: {original_name}")
+                    custom_names.append(original_name)
+                else:
+                    labels.update(matching)
+        merged.extensions = [e for e in merged.extensions if not re.match("OriginalName:(.*)", e)]
+        print(f"remaining extensions: {merged.extensions}")
+
+        track = gpxpy.gpx.GPXTrack()
+        track.segments.append(merged)
+
+        track.name = " ".join(
+            [str(start)] + custom_names + [f"{k}: {v}" for k, v in labels.most_common()]
+        )
+        track.description = "\n".join(sorted(original_names))
+        output_gpx.tracks.append(track)
+
+    print(f"final tracks: {len(output_gpx.tracks)}")
+    # make sure all of the points in the input made it through to one of the merged segments
+    print(f"all: {len(all_points)}, merged: {len(merged_points)}")
+    print(f"all points appear in merge: {all_points == merged_points}")
+
+    return output_gpx
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="Path to GPX file to process")
+    parser.add_argument("output", help="Path to write processed GPX file")
+    args = parser.parse_args()
+
+    with open(args.input, 'r') as gpx_file:
+        print(f"Loading {args.input}")
+        input_gpx = gpxpy.parse(gpx_file)
+
+    output_gpx = gpx_merge_intersect(input_gpx)
+    with open(args.output, 'w') as gpx_file:
+        print(output_gpx.to_xml(), file=gpx_file)
---
+++ organize-tracks-by-local-day.py
@@ -0,0 +1,87 @@
+import argparse
+import os
+from collections import defaultdict
+
+import gpxpy.gpx
+import requests
+import dateutil.parser
+
+from secrets import azure_key
+
+
+def first_point_with_time(gpx_track):
+    for segment in gpx_track.segments:
+        for point in segment.points:
+            if point.time:
+                return point
+
+
+def localtime_for_point(gpx_point):
+    params = {
+        "subscription-key": azure_key,
+        "api-version": "1.0",
+        "options": "all",
+        "query": f"{gpx_point.latitude},{gpx_point.longitude}",
+        "timeStamp": gpx_point.time.isoformat() + "Z"
+    }
+    r = requests.get("https://atlas.microsoft.com/timezone/byCoordinates/json", params=params)
+    r.raise_for_status()
+    return dateutil.parser.parse(r.json()["TimeZones"][0]["ReferenceTime"]["WallTime"])
+
+
+def organize_by_local_day(input_gpx):
+    """Returns a dict of string date -> [(time, track)]"""
+
+    tracks_by_date = defaultdict(list)
+    for track in input_gpx.tracks:
+        date = "Unknown"
+        time = None
+        try:
+            point = first_point_with_time(track)
+            time = point.time  # UTC!
+            local = localtime_for_point(point)
+            date = local.date().isoformat()
+            track.name = f"{local} {track.name}"
+        finally:
+            print(f"Added track at {time}")
+            tracks_by_date[date].append((time, track))
+    return tracks_by_date
+
+
+def sort_tracks_into_gpx(tracks_by_date):
+    gpx_by_date = {}
+    for date, time_tracks in sorted(tracks_by_date.items(), key=lambda t: t[0]):
+        output_gpx = gpxpy.gpx.GPX()
+        output_gpx.tracks = []
+        sorted_time_tracks = sorted(time_tracks, key=lambda t: t[0])
+        print(f"Sorting {date}\n" + "\n".join([f"  {ti} {tr.name}" for ti,tr in sorted_time_tracks]))
+        for time, track in sorted_time_tracks:
+            track.extensions = None  # TODO gpxpy doesn't deal with garmin extensions properly
+            output_gpx.tracks.append(track)
+        gpx_by_date[date] = output_gpx
+
+    return gpx_by_date
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", nargs='+', help="Path to GPX file to process")
+    parser.add_argument("output_dir", help="Path to directory to put resulting files in")
+    args = parser.parse_args()
+
+    dir = os.path.abspath(args.output_dir)
+    os.makedirs(dir, exist_ok=True)
+
+    all_tracks_by_date = defaultdict(list)
+    for input_path in args.input:
+        with open(input_path, "r") as gpx_file:
+            print(f"Loading {input_path}")
+            input_gpx = gpxpy.parse(gpx_file)
+            for date, tracks in organize_by_local_day(input_gpx).items():
+                all_tracks_by_date[date].extend(tracks)
+
+    gpx_by_date = sort_tracks_into_gpx(all_tracks_by_date)
+    for date, gpx in gpx_by_date.items():
+        output_path = os.path.join(dir, f"{date}.gpx")
+        with open(output_path, "w") as out_file:
+            print(gpx.to_xml(), file=out_file)
